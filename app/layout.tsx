4416a0c127225c39eb14d3aa9e5e6a92db9d814c
rsovkovic <rsovkovic@gmail.com>
2025-08-08 18:40:26 +0300

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
-import TanStackProvider from "@/components/TanstackProvider/TanstackProvider";
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
